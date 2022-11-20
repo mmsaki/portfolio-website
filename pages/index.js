60c38ffe8c6dfc76da31fcad5d83ac7bceb5713c
mmsaki <meek10x@gmail.com>
2022-11-19 20:46:26 -0600

    feature website

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import utilStyles from '../styles/utils.module.css' 
+import Link from 'next/link'
+import Layout from '../components/layout'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hello, I'm <span className={utilStyles.bolded}>Meek. </span>
+          I'm a Blockchain Analytics Engineer and an Artist (Sculptor & Music Producer).
+          You can contact me on
+          {' '}
+          <a href="https://twitter.com/msakiart">Twitter</a>.
+        </p>
+      </section>     
+    </Layout>
+  )
+}
