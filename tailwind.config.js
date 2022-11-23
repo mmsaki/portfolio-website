6fb95d8c8663473f7f25c3d672b7a7da1f7c1aa1
mmsaki <meek10x@gmail.com>
2022-11-23 04:42:43 -0600

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,91 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+  darkMode: "class",
   content: [
-    "./app/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./posts/**/*.{md,mdx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      blue: "#1fb6ff",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      typography: ({ theme }) => ({
+        pink: {
+          css: {
+            "--tw-prose-body": theme("colors.pink[800]"),
+            "--tw-prose-headings": theme("colors.pink[900]"),
+            "--tw-prose-lead": theme("colors.pink[700]"),
+            "--tw-prose-links": theme("colors.pink[900]"),
+            "--tw-prose-bold": theme("colors.pink[900]"),
+            "--tw-prose-counters": theme("colors.pink[600]"),
+            "--tw-prose-bullets": theme("colors.pink[400]"),
+            "--tw-prose-hr": theme("colors.pink[300]"),
+            "--tw-prose-quotes": theme("colors.pink[900]"),
+            "--tw-prose-quote-borders": theme("colors.pink[300]"),
+            "--tw-prose-captions": theme("colors.pink[700]"),
+            "--tw-prose-code": theme("colors.pink[900]"),
+            "--tw-prose-pre-code": theme("colors.pink[100]"),
+            "--tw-prose-pre-bg": theme("colors.pink[900]"),
+            "--tw-prose-th-borders": theme("colors.pink[300]"),
+            "--tw-prose-td-borders": theme("colors.pink[200]"),
+            "--tw-prose-invert-body": theme("colors.pink[200]"),
+            "--tw-prose-invert-headings": theme("colors.white"),
+            "--tw-prose-invert-lead": theme("colors.pink[300]"),
+            "--tw-prose-invert-links": theme("colors.white"),
+            "--tw-prose-invert-bold": theme("colors.white"),
+            "--tw-prose-invert-counters": theme("colors.pink[400]"),
+            "--tw-prose-invert-bullets": theme("colors.pink[600]"),
+            "--tw-prose-invert-hr": theme("colors.pink[700]"),
+            "--tw-prose-invert-quotes": theme("colors.pink[100]"),
+            "--tw-prose-invert-quote-borders": theme("colors.pink[700]"),
+            "--tw-prose-invert-captions": theme("colors.pink[400]"),
+            "--tw-prose-invert-code": theme("colors.white"),
+            "--tw-prose-invert-pre-code": theme("colors.pink[300]"),
+            "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)",
+            "--tw-prose-invert-th-borders": theme("colors.pink[600]"),
+            "--tw-prose-invert-td-borders": theme("colors.pink[700]"),
+            color: "#333",
+            a: {
+              color: "#3182ce",
+              "&:hover": {
+                color: "#2c5282",
+              },
+            },
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography")({
+      className: "prose",
+      strategy: "base",
+    }),
+  ],
 };
