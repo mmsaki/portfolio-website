5c42f0d416978e915b7e30482fdc683c314e2944
mmsaki <meek10x@gmail.com>
2022-11-23 15:00:24 -0600

    command pallete test

---
+++ components/CommandPallet.js
@@ -0,0 +1,9 @@
+export default function CommadPallete() {
+  return (
+    <div className="fixed inset-x-0 top-0 bg-purple-200">
+      <p className="py-4 text-center text-purple-900">
+        Let's buidl a command pallet!
+      </p>
+    </div>
+  );
+}
