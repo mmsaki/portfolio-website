b494c2b52c3cacb3e23b4f394ae2a48d372a21b6
Meek <meek10x@gmail.com>
2022-12-21 20:32:12 -0600

    porfolio pdf

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,33 +1,30 @@
 import Head from "next/head";
-import Image from "next/image";
-import utilStyles from "../../styles/utils.module.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef } from 'react';
 
-export default function Portfolio() {
-    const viewer = useRef(null);
+export default function Porfolio() {
+  const viewer = useRef(null);
 
   useEffect(() => {
     import('@pdftron/webviewer').then(() => {
       WebViewer(
         {
-          path: "/lib",
+          path: '/lib',
           initialDoc: "/files/portfolio_oct_2022.pdf",
         },
         viewer.current
       ).then((instance) => {
         const { docViewer } = instance;
-        //
+        // you can now call WebViewer APIs here...
       });
     });
-  },
-    []);
+  }, []);
 
   return (
-    <div className="container">
+    <div className='container'>
       <Head>
         <title>Meek Msaki Resume</title>
       </Head>
-      <div className="webviewer" ref={viewer} style={{ height: "100vh", width: "100vw" }}></div>
+      <div className='webviewer' ref={viewer} style={{ height: '100vh', width: "100vh" }}></div>
     </div>
   );
 }
